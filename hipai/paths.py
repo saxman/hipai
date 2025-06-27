3f003f09a041787cb6980b9c7a03cc52c9e7c12f
Paul R Saxman <saxman@gmail.com>
2025-06-26 19:13:42 -0700

    Add paths.py to define project directory paths

---
+++ hipai/paths.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+root = Path(__file__).parents[1]
+
+# Path to the data directory
+data = root / "data"
+
+# Path to the tests directory
+tests = root / "tests"
+
+# Path to the package directory
+package = root / "hipai"
