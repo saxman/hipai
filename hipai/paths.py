e8a809fd6b6640c4219456fca928a01c40e64887
Paul R Saxman <saxman@gmail.com>
2025-07-17 22:27:27 -0700

    Clarify comments in paths.py for data, tests, package, and output directories

--- hipai/paths.py
+++ hipai/paths.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 root = Path(__file__).parents[1]
 
-# Path to the data directory
+# Path to the input data directory
 data = root / "data"
 
 # Path to the tests directory
@@ -10,3 +10,6 @@ tests = root / "tests"
 
 # Path to the package directory
 package = root / "hipai"
+
+# Path to the output directory
+output = root / "output"
